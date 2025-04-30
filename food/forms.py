fb3dd5985bd6f7c91d9589f6071ed899026c6fe3
NMMalika <94176433+NMMalika@users.noreply.github.com>
2025-04-30 17:07:11 +0300

    bootsrap form using  widget

--- food/forms.py
+++ food/forms.py
@@ -1,8 +1,57 @@
 from django.forms import ModelForm
 from .models import Booktable
+from django import forms
 
 class Booktableform (ModelForm):
     class Meta:
         model = Booktable
         fields = ['name', 'email', 'phone', 'date', 'time', 'guests', 'message']
+        labels = {
+            'name': '',
+            'email': '',
+            'phone': '',
+            'date': '',
+            'time': '',
+            'guests': '',
+            'message': '',
+        }
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Name',
+        'required': 'required'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Email',
+        'required': 'required'
+    }))
+    phone = forms.CharField(max_length=15, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Mobile',
+        'required': 'required'
+    }))
+    date = forms.DateField(widget=forms.TextInput(attrs={
+        'class': 'form-control datetimepicker-input',
+        'placeholder': 'Date',
+        'data-toggle': 'datetimepicker',
+        'data-target': '#date'
+    }))
+    time = forms.TimeField(widget=forms.TextInput(attrs={
+        'class': 'form-control datetimepicker-input',
+        'placeholder': 'Time',
+        'data-toggle': 'datetimepicker',
+        'data-target': '#time'
+    }))
+    guests = forms.ChoiceField(choices=[(str(i), f"{i} Guest") for i in range(1, 11)], widget=forms.Select(attrs={
+        'class': 'custom-select form-control'
+    }))
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'placeholder': 'Message',
+        'rows': 4 ,
+        'size': '40'
+        
+    }))
+   
+    
         
--- food/views.py
+++ food/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from food.models import Contact, Items, ItemList, Booktable
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .forms import Booktableform
 
 # Create your views here.
@@ -38,10 +38,16 @@ def feature(request):
 def booking(request):
     
   forms=Booktableform()
+  submit=False
   if request.method=='POST':
-      print(request.POST)
+     
       forms=Booktableform(request.POST)
       if forms.is_valid():
           forms.save()
-  context={"form":forms}
+          return HttpResponseRedirect('/booking?submit=True')
+  else:
+      forms=Booktableform()
+      if 'submit' in request.GET:
+          submit=True
+  context={"form":forms,"submit":submit}
   return render(request,'booking.html',context)
